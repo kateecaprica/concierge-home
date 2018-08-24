c0a93b118a73340f3db9540c44b064c77dc3abf2
Zach Mumbauer <mumbeezy@gmail.com>
2018-08-24 00:43:23 -0400

    changed phone number

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 			  			<div class="col-lg-6 col-sm-6 col-4 header-top-right no-padding">
-			  				<a href="tel:+1-202-235-2022"><span class="lnr lnr-phone-handset"></span> <span class="text">+1-202-235-2022</span></a>
+			  				<a href="tel:+1-202-235-2399"><span class="lnr lnr-phone-handset"></span> <span class="text">+1-202-235-2399</span></a>
 			  				<a href="mailto:support@symbolicconcierge.com"><span class="lnr lnr-envelope"></span> <span class="text">support@symbolicconcierge.com</span></a>		
 			  			</div>
-			  					<script>
-function post(path, params, method) {
+                      <script>
+                        function post(path, params, method) {
     method = method || "post"; // Set method to post by default if not specified.
 
     // The rest of this code assumes you are not using a library.
@@ -26,4 +26,4 @@ function post(path, params, method) {
     document.body.appendChild(form);
     form.submit();
 }
-		</script>
+</script>
